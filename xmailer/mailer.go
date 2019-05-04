7fa4ee8871ba5aac67852252c812c4e0f7ed0ace
Li Kexian <i@likexian.com>
2019-05-04 20:10:06 +0800

    import mailer-go: Fri Feb 15 14:17:19 2019 +0800: do gofmt

--- xmailer/mailer.go
+++ xmailer/mailer.go
@@ -9,172 +9,160 @@
 
 package mailer
 
-
 import (
-    "fmt"
-    "time"
-    "strings"
-    "bytes"
-    "encoding/base64"
-    "crypto/md5"
-    "path/filepath"
-    "io/ioutil"
-    "net/smtp"
+	"bytes"
+	"crypto/md5"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"net/smtp"
+	"path/filepath"
+	"strings"
+	"time"
 )
-
 
 // Attachment storing mail attachment
 type Attachment struct {
-    Name        string
-    Content     []byte
+	Name    string
+	Content []byte
 }
-
 
 // Auth storing mail auth
 type Auth struct {
-    Server      string
-    Auth        smtp.Auth
+	Server string
+	Auth   smtp.Auth
 }
-
 
 // Message storing mail message
 type Message struct {
-    From        string
-    To          []string
-    Cc          []string
-    Bcc         []string
-    Subject     string
-    Body        string
-    ContentType string
-    Attachments map[string]*Attachment
-    Auth        *Auth
+	From        string
+	To          []string
+	Cc          []string
+	Bcc         []string
+	Subject     string
+	Body        string
+	ContentType string
+	Attachments map[string]*Attachment
+	Auth        *Auth
 }
-
 
 // Version returns package version
 func Version() string {
-    return "0.3.0"
+	return "0.3.0"
 }
-
 
 // Author returns package author
 func Author() string {
-    return "[Li Kexian](https://www.likexian.com/)"
+	return "[Li Kexian](https://www.likexian.com/)"
 }
-
 
 // License returns package license
 func License() string {
-    return "Apache License, Version 2.0"
+	return "Apache License, Version 2.0"
 }
-
 
 // New returns a new mailer
 func New(server, username, password string, ishtml bool) (m *Message) {
-    m = &Message{
-        From: username,
-        To: []string{username},
-        Subject: "Mailer Test",
-        Body: "Hello World. This is mailer via github.com/likexian/mailer-go.",
-        Attachments: make(map[string]*Attachment),
-    }
+	m = &Message{
+		From:        username,
+		To:          []string{username},
+		Subject:     "Mailer Test",
+		Body:        "Hello World. This is mailer via github.com/likexian/mailer-go.",
+		Attachments: make(map[string]*Attachment),
+	}
 
-    if ishtml {
-        m.ContentType = "text/html"
-    } else {
-        m.ContentType = "text/plain"
-    }
+	if ishtml {
+		m.ContentType = "text/html"
+	} else {
+		m.ContentType = "text/plain"
+	}
 
-    servers := strings.Split(server, ":")
-    m.Auth = &Auth {
-        Server: server,
-        Auth: smtp.PlainAuth("", username, password, servers[0]),
-    }
+	servers := strings.Split(server, ":")
+	m.Auth = &Auth{
+		Server: server,
+		Auth:   smtp.PlainAuth("", username, password, servers[0]),
+	}
 
-    return
+	return
 }
-
 
 // Attach add a attachment
 func (m *Message) Attach(fname string) (err error) {
-    data, err := ioutil.ReadFile(fname)
-    if err != nil {
-        return
-    }
+	data, err := ioutil.ReadFile(fname)
+	if err != nil {
+		return
+	}
 
-    _, name := filepath.Split(fname)
-    m.Attachments[name] = &Attachment {
-        Name: name,
-        Content: data,
-    }
+	_, name := filepath.Split(fname)
+	m.Attachments[name] = &Attachment{
+		Name:    name,
+		Content: data,
+	}
 
-    return
+	return
 }
-
 
 // Send do the sending
 func (m *Message) Send() (err error) {
-    return smtp.SendMail(m.Auth.Server, m.Auth.Auth, m.From, m.innerTo(), m.innerBody())
+	return smtp.SendMail(m.Auth.Server, m.Auth.Auth, m.From, m.innerTo(), m.innerBody())
 }
-
 
 // innerTo returns mail receipt
 func (m *Message) innerTo() (to []string) {
-    to = m.To
-    for _, v := range m.Cc {
-        to = append(to, v)
-    }
+	to = m.To
+	for _, v := range m.Cc {
+		to = append(to, v)
+	}
 
-    for _, v := range m.Bcc {
-        to = append(to, v)
-    }
+	for _, v := range m.Bcc {
+		to = append(to, v)
+	}
 
-    return
+	return
 }
-
 
 // innerBody returns mail body
 func (m *Message) innerBody() (body []byte) {
-    now := time.Now()
-    date := now.Format(time.RFC822)
-    buf := bytes.NewBuffer(nil)
-    boundary := "----=_NextPart_" + fmt.Sprintf("%x", md5.Sum([]byte(date)))[:16]
+	now := time.Now()
+	date := now.Format(time.RFC822)
+	buf := bytes.NewBuffer(nil)
+	boundary := "----=_NextPart_" + fmt.Sprintf("%x", md5.Sum([]byte(date)))[:16]
 
-    buf.WriteString("Date: " + date + "\r\n")
-    buf.WriteString("From: " + m.From + "\r\n")
-    buf.WriteString("To: " + strings.Join(m.To, ",") + "\r\n")
-    if len(m.Cc) > 0 {
-        buf.WriteString("Cc: " + strings.Join(m.Cc, ",") + "\r\n")
-    }
-    buf.WriteString("Subject: " + m.Subject + "\r\n")
-    buf.WriteString("X-Priority: 3\r\n")
-    buf.WriteString("MIME-Version: 1.0\r\n")
-    buf.WriteString("X-Mailer: github.com/likexian/mailer-go\r\n")
-    buf.WriteString("Content-Type: multipart/mixed; boundary=" + boundary + "\r\n\r\n")
+	buf.WriteString("Date: " + date + "\r\n")
+	buf.WriteString("From: " + m.From + "\r\n")
+	buf.WriteString("To: " + strings.Join(m.To, ",") + "\r\n")
+	if len(m.Cc) > 0 {
+		buf.WriteString("Cc: " + strings.Join(m.Cc, ",") + "\r\n")
+	}
+	buf.WriteString("Subject: " + m.Subject + "\r\n")
+	buf.WriteString("X-Priority: 3\r\n")
+	buf.WriteString("MIME-Version: 1.0\r\n")
+	buf.WriteString("X-Mailer: github.com/likexian/mailer-go\r\n")
+	buf.WriteString("Content-Type: multipart/mixed; boundary=" + boundary + "\r\n\r\n")
 
-    buf.WriteString("This is a multi-part message in MIME format.\r\n")
-    buf.WriteString("\r\n--" + boundary + "\r\n")
-    buf.WriteString("Content-Type: " + m.ContentType + "; charset=utf-8\r\n\r\n")
-    buf.WriteString(m.Body)
-    buf.WriteString("\r\n")
+	buf.WriteString("This is a multi-part message in MIME format.\r\n")
+	buf.WriteString("\r\n--" + boundary + "\r\n")
+	buf.WriteString("Content-Type: " + m.ContentType + "; charset=utf-8\r\n\r\n")
+	buf.WriteString(m.Body)
+	buf.WriteString("\r\n")
 
-    if len(m.Attachments) > 0 {
-        for _, attachment := range m.Attachments {
-            buf.WriteString("\r\n--" + boundary + "\r\n")
-            buf.WriteString("Content-Type: application/octet-stream\r\n")
-            buf.WriteString("Content-Transfer-Encoding: base64\r\n")
-            buf.WriteString("Content-ID: <" + attachment.Name + ">\r\n")
-            buf.WriteString("Content-Disposition: attachment; filename=\"" + attachment.Name + "\"\r\n\r\n")
+	if len(m.Attachments) > 0 {
+		for _, attachment := range m.Attachments {
+			buf.WriteString("\r\n--" + boundary + "\r\n")
+			buf.WriteString("Content-Type: application/octet-stream\r\n")
+			buf.WriteString("Content-Transfer-Encoding: base64\r\n")
+			buf.WriteString("Content-ID: <" + attachment.Name + ">\r\n")
+			buf.WriteString("Content-Disposition: attachment; filename=\"" + attachment.Name + "\"\r\n\r\n")
 
-            data := make([]byte, base64.StdEncoding.EncodedLen(len(attachment.Content)))
-            base64.StdEncoding.Encode(data, attachment.Content)
-            buf.Write(data)
-            buf.WriteString("\r\n")
-        }
-    }
+			data := make([]byte, base64.StdEncoding.EncodedLen(len(attachment.Content)))
+			base64.StdEncoding.Encode(data, attachment.Content)
+			buf.Write(data)
+			buf.WriteString("\r\n")
+		}
+	}
 
-    buf.WriteString("\r\n--" + boundary + "--\r\n")
-    body = buf.Bytes()
+	buf.WriteString("\r\n--" + boundary + "--\r\n")
+	body = buf.Bytes()
 
-    return
+	return
 }
--- xmailer/mailer_test.go
+++ xmailer/mailer_test.go
@@ -9,45 +9,43 @@
 
 package mailer
 
-
 import (
-    "fmt"
-    "testing"
+	"fmt"
+	"testing"
 )
 
-
 func TestMailer(t *testing.T) {
-    // Set the smtp info
-    m := New("smtp.likexian.com:25", "i@likexian.com", "8Bd0a7681333214", true)
+	// Set the smtp info
+	m := New("smtp.likexian.com:25", "i@likexian.com", "8Bd0a7681333214", true)
 
-    // Set email from
-    m.From = "i@likexian.com"
+	// Set email from
+	m.From = "i@likexian.com"
 
-    // Set send to
-    m.To = []string{"i@likexian.com"}
+	// Set send to
+	m.To = []string{"i@likexian.com"}
 
-    // Set mail subject
-    m.Subject = "Mailer Test"
+	// Set mail subject
+	m.Subject = "Mailer Test"
 
-    // Set mail body
-    m.Body = "Hello World. This is mailer via github.com/likexian/mailer-go.<br /><img src=\"cid:mailer_test.jpg\" />"
+	// Set mail body
+	m.Body = "Hello World. This is mailer via github.com/likexian/mailer-go.<br /><img src=\"cid:mailer_test.jpg\" />"
 
-    // Add attachment
-    err := m.Attach("mailer_test.jpg")
-    if err != nil {
-        t.Error(err)
-    }
+	// Add attachment
+	err := m.Attach("mailer_test.jpg")
+	if err != nil {
+		t.Error(err)
+	}
 
-    if _, ok := m.Attachments["mailer_test.jpg"]; !ok {
-        t.Error("Fail to add attachment")
-    }
+	if _, ok := m.Attachments["mailer_test.jpg"]; !ok {
+		t.Error("Fail to add attachment")
+	}
 
-    // The smtp info is fake, sending will never success.
-    err = m.Send()
-    if (err != nil) {
-        // t.Error(err)
-        fmt.Println("There is something WRONG:")
-        fmt.Println(err)
-        fmt.Println("")
-    }
+	// The smtp info is fake, sending will never success.
+	err = m.Send()
+	if err != nil {
+		// t.Error(err)
+		fmt.Println("There is something WRONG:")
+		fmt.Println(err)
+		fmt.Println("")
+	}
 }
