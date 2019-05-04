0c52d7bac89f381544549fd4edd8806b8592cebe
Li Kexian <i@likexian.com>
2019-05-04 20:34:50 +0800

    import logger-go: Fri Feb 15 14:26:21 2019 +0800: do gofmt

--- logger/logger.go
+++ logger/logger.go
@@ -9,163 +9,143 @@
 
 package logger
 
-
 import (
-    "fmt"
-    "strings"
-    "time"
-    "sync"
-    "io"
-    "os"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"sync"
+	"time"
 )
-
 
 // Level storing log level
 type Level int
 
-
 // Logger storing logger
 type Logger struct {
-    Writer io.Writer
-    Level Level
-    sync.Mutex
+	Writer io.Writer
+	Level  Level
+	sync.Mutex
 }
-
 
 // Log level const
 const (
-    DEBUG Level = iota
-    INFO
-    NOTICE
-    WARNING
-    ERROR
-    CRITICAL
+	DEBUG Level = iota
+	INFO
+	NOTICE
+	WARNING
+	ERROR
+	CRITICAL
 )
 
-
 // log level mapper
-var levels = map[string]Level {
-    "debug": DEBUG,
-    "info": INFO,
-    "notice": NOTICE,
-    "warning": WARNING,
-    "error": ERROR,
-    "critical": CRITICAL,
+var levels = map[string]Level{
+	"debug":    DEBUG,
+	"info":     INFO,
+	"notice":   NOTICE,
+	"warning":  WARNING,
+	"error":    ERROR,
+	"critical": CRITICAL,
 }
-
 
 // Version returns package version
 func Version() string {
-    return "0.3.0"
+	return "0.3.0"
 }
-
 
 // Author returns package author
 func Author() string {
-    return "[Li Kexian](https://www.likexian.com/)"
+	return "[Li Kexian](https://www.likexian.com/)"
 }
-
 
 // License returns package license
 func License() string {
-    return "Apache License, Version 2.0"
+	return "Apache License, Version 2.0"
 }
-
 
 // New returns a new logger
 func New(w io.Writer, level Level) *Logger {
-    l := &Logger{Writer: w, Level: level}
-    return l
+	l := &Logger{Writer: w, Level: level}
+	return l
 }
-
 
 // File returns a new file logger
 func File(fname string, level Level) (*Logger, error) {
-    fd, err := os.OpenFile(fname, os.O_CREATE | os.O_WRONLY | os.O_APPEND, 0644)
-    if err != nil {
-        return nil, err
-    }
-    return New(fd, level), nil
+	fd, err := os.OpenFile(fname, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return New(fd, level), nil
 }
-
 
 // SetLevel set the log level by int level
 func (l *Logger) SetLevel(level Level) {
-    l.Lock()
-    defer l.Unlock()
-    l.Level = level
+	l.Lock()
+	defer l.Unlock()
+	l.Level = level
 }
-
 
 // SetLevelString set the log level by string level
 func (l *Logger) SetLevelString(level string) error {
-    value := l.GetLevelByString(level)
-    if value >= 0 {
-        l.SetLevel(value)
-    }
-    return fmt.Errorf("%s is invalid level", level)
+	value := l.GetLevelByString(level)
+	if value >= 0 {
+		l.SetLevel(value)
+	}
+	return fmt.Errorf("%s is invalid level", level)
 }
-
 
 // GetLevelByString returns log level by string level
 func (l *Logger) GetLevelByString(level string) Level {
-    level = strings.ToLower(level)
-    if value, ok := levels[level]; ok {
-        return value
-    }
-    return -1
+	level = strings.ToLower(level)
+	if value, ok := levels[level]; ok {
+		return value
+	}
+	return -1
 }
-
 
 // Log do log a msg
 func (l *Logger) Log(level string, msg string, args ...interface{}) error {
-    value := l.GetLevelByString(level)
-    if l.Level > value {
-        return nil
-    }
+	value := l.GetLevelByString(level)
+	if l.Level > value {
+		return nil
+	}
 
-    now := time.Now().Format("2006-01-02 15:04:05")
-    str := fmt.Sprintf("%s [%s] %s\n", now, strings.ToUpper(level), msg)
+	now := time.Now().Format("2006-01-02 15:04:05")
+	str := fmt.Sprintf("%s [%s] %s\n", now, strings.ToUpper(level), msg)
 
-    l.Lock()
-    _, err := fmt.Fprintf(l.Writer, str, args...)
-    l.Unlock()
+	l.Lock()
+	_, err := fmt.Fprintf(l.Writer, str, args...)
+	l.Unlock()
 
-    return err
+	return err
 }
-
 
 // Debug level msg logging
 func (l *Logger) Debug(msg string, args ...interface{}) error {
-    return l.Log("DEBUG", msg, args...)
+	return l.Log("DEBUG", msg, args...)
 }
-
 
 // Info level msg logging
 func (l *Logger) Info(msg string, args ...interface{}) error {
-    return l.Log("INFO", msg, args...)
+	return l.Log("INFO", msg, args...)
 }
-
 
 // Notice level msg logging
 func (l *Logger) Notice(msg string, args ...interface{}) error {
-    return l.Log("NOTICE", msg, args...)
+	return l.Log("NOTICE", msg, args...)
 }
-
 
 // Warning level msg logging
 func (l *Logger) Warning(msg string, args ...interface{}) error {
-    return l.Log("WARNING", msg, args...)
+	return l.Log("WARNING", msg, args...)
 }
-
 
 // Error level msg logging
 func (l *Logger) Error(msg string, args ...interface{}) error {
-    return l.Log("ERROR", msg, args...)
+	return l.Log("ERROR", msg, args...)
 }
-
 
 // Critical level msg logging
 func (l *Logger) Critical(msg string, args ...interface{}) error {
-    return l.Log("CRITICAL", msg, args...)
+	return l.Log("CRITICAL", msg, args...)
 }
--- logger/logger_test.go
+++ logger/logger_test.go
@@ -9,41 +9,39 @@
 
 package logger
 
-
 import (
-    "os"
-    "testing"
+	"os"
+	"testing"
 )
 
-
 func TestLogger(t *testing.T) {
-    log := New(os.Stderr, DEBUG)
-    log.Debug("This is Debug")
-    log.Info("This is Info")
-    log.Notice("This is Notice")
-    log.Warning("This is Warning")
-    log.Error("This is Error")
-    log.Critical("This is Critical")
+	log := New(os.Stderr, DEBUG)
+	log.Debug("This is Debug")
+	log.Info("This is Info")
+	log.Notice("This is Notice")
+	log.Warning("This is Warning")
+	log.Error("This is Error")
+	log.Critical("This is Critical")
 
-    log.Info("Now setting level to Info")
-    log.SetLevel(INFO)
-    log.Debug("This is Debug")
-    log.Info("This is Info")
+	log.Info("Now setting level to Info")
+	log.SetLevel(INFO)
+	log.Debug("This is Debug")
+	log.Info("This is Info")
 
-    log.SetLevel(INFO)
-    log.Info("Now setting level to Error")
-    log.SetLevelString("Error")
-    log.Warning("This is Warning")
-    log.Error("This is Error")
+	log.SetLevel(INFO)
+	log.Info("Now setting level to Error")
+	log.SetLevelString("Error")
+	log.Warning("This is Warning")
+	log.Error("This is Error")
 
-    flog, err := File("test.log", DEBUG)
-    if err != nil {
-        panic(err)
-    }
-    flog.Debug("This is Debug")
-    flog.Info("This is Info")
-    flog.Notice("This is Notice")
-    flog.Warning("This is Warning")
-    flog.Error("This is Error")
-    flog.Critical("This is Critical")
+	flog, err := File("test.log", DEBUG)
+	if err != nil {
+		panic(err)
+	}
+	flog.Debug("This is Debug")
+	flog.Info("This is Info")
+	flog.Notice("This is Notice")
+	flog.Warning("This is Warning")
+	flog.Error("This is Error")
+	flog.Critical("This is Critical")
 }
