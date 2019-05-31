a3d3cdf9fe7c74902be11544b0d19f488da0172b
Li Kexian <i@likexian.com>
2019-05-31 22:51:00 +0800

    add pid kits to xos

---
+++ xos/pid.go
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2012-2019 Li Kexian
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * A toolkit for Golang development
+ * https://www.likexian.com/
+ */
+
+package xos
+
+import (
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+	"syscall"
+
+	"github.com/likexian/gokit/xfile"
+)
+
+// Pid storing pid info
+type Pid struct {
+	file string
+}
+
+var (
+	// ErrPidExists is pid file exists
+	ErrPidExists = errors.New("xos: process is running")
+	// ErrPidLockFailed is pid lock failed
+	ErrPidLockFailed = errors.New("xos: lock pid file failed")
+)
+
+// New init a new pid
+func New(f string) *Pid {
+	return &Pid{f}
+}
+
+// Create check create a new pid file
+func (p *Pid) Create() (int, error) {
+	if xfile.Exists(p.file) {
+		pid, err := p.Alive()
+		if err == nil {
+			return pid, ErrPidExists
+		}
+	}
+
+	fd, err := xfile.New(p.file)
+	if err != nil {
+		return 0, err
+	}
+
+	defer fd.Close()
+	pid := os.Getpid()
+	fd.Write([]byte(strconv.Itoa(pid)))
+
+	err = syscall.Flock(int(fd.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
+	if err == syscall.EWOULDBLOCK {
+		return pid, ErrPidLockFailed
+	}
+
+	return pid, err
+}
+
+// Alive returns pid is alive
+func (p *Pid) Alive() (int, error) {
+	pid, err := p.Value()
+	if err != nil {
+		return 0, err
+	}
+
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		return pid, err
+	}
+
+	err = process.Signal(os.Signal(syscall.Signal(0)))
+	if err != nil {
+		return pid, err
+	}
+
+	return pid, nil
+}
+
+// Value returns pid value
+func (p *Pid) Value() (int, error) {
+	text, err := xfile.ReadText(p.file)
+	if err != nil {
+		return 0, err
+	}
+
+	return strconv.Atoi(strings.TrimSpace(text))
+}
---
+++ xos/pid_test.go
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2012-2019 Li Kexian
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * A toolkit for Golang development
+ * https://www.likexian.com/
+ */
+
+package xos
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/likexian/gokit/assert"
+	"github.com/likexian/gokit/xfile"
+)
+
+const (
+	pidFile = "/tmp/testing.pid"
+)
+
+func TestValue(t *testing.T) {
+	defer os.Remove(pidFile)
+
+	p := New(pidFile)
+	_, err := p.Value()
+	assert.NotNil(t, err)
+
+	err = xfile.WriteText(pidFile, "1")
+	assert.Nil(t, err)
+
+	pid, err := p.Value()
+	assert.Nil(t, err)
+	assert.Equal(t, pid, 1)
+}
+
+func TestAlive(t *testing.T) {
+	defer os.Remove(pidFile)
+
+	p := New(pidFile)
+	_, err := p.Alive()
+	assert.NotNil(t, err)
+
+	err = xfile.WriteText(pidFile, "88888888")
+	assert.Nil(t, err)
+
+	_, err = p.Alive()
+	assert.NotNil(t, err)
+
+	err = xfile.WriteText(pidFile, "1")
+	assert.Nil(t, err)
+
+	pid, err := p.Alive()
+	assert.Nil(t, err)
+	assert.Equal(t, pid, 1)
+}
+
+func TestCreate(t *testing.T) {
+	defer os.Remove(pidFile)
+
+	err := xfile.WriteText(pidFile, "1")
+	assert.Nil(t, err)
+
+	p := New(pidFile)
+	pid, err := p.Create()
+	assert.NotNil(t, err)
+	assert.Equal(t, pid, 1)
+
+	err = xfile.WriteText(pidFile, "88888888")
+	assert.Nil(t, err)
+
+	_, err = p.Create()
+	assert.Nil(t, err)
+}
+
+func TestConcurrency(t *testing.T) {
+	defer os.Remove(pidFile)
+
+	p := New(pidFile)
+	for i := 0; i < 1000; i++ {
+		go func() {
+			_, err := p.Create()
+			if err != nil {
+				if err != ErrPidLockFailed && err != ErrPidExists {
+					t.Errorf("Unexcepted error: %s", err)
+				}
+			}
+		}()
+	}
+
+	time.Sleep(1 * time.Second)
+}
